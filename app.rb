f4a1bfbdb9f8f84831adf0c900e4f8493df1c18b
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-25 17:04:36 +0100

    hello world2

--- app.rb
+++ app.rb
@@ -3,11 +3,5 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
   get '/' do
-    'Hello world!'
-  end
-
-  get '/cat' do
-    "<div style='border: 3px dashed red'>
-       <img src='https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg'>
-     </div>"
+    erb(:index)
   end
