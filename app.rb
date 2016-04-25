d7ca705e05d6d91f76e29a63c780e507b7eb9cd5
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-25 17:32:36 +0100

    hello world2

--- app.rb
+++ app.rb
@@ -3,5 +3,17 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
   get '/' do
+    "<h1>Hello World</h1>"
+  end
+
+
+  get '/random-cat' do
+    @name = ["Amigo", "Oscar", "Viking"].sample
+    erb(:index)
+  end
+
+  get '/named-cat' do
+    p params
+    @name = params[:name]
     erb(:index)
   end
